8d9269dd90caf82f887e7c812de55731641f5e64
Patricco441 <phachamowski@gmail.com>
2023-09-30 17:35:41 +0200

    click handler

--- src/pages/ApplicationPage/components/SidenavWrapper/SidenavWrapper.tsx
+++ src/pages/ApplicationPage/components/SidenavWrapper/SidenavWrapper.tsx
@@ -1,13 +1,32 @@
-import styles from './SidenavWrapper.module.css'
+import { useCallback } from 'react';
+import { Marker, useMapEvents } from 'react-leaflet';
+import styles from './SidenavWrapper.module.css';
 
-type Props = {}
+type Props = {};
 
 const SidenavWrapper = (props: Props) => {
+  // // const map = useMapEvents({
+  // //   click(e) {
+  // //     // Add marker to clicked location
+  // //   },
+  // // });
+  
+  // const handleAddMarker = useCallback(() => {
+  //   // Set map click handler 
+  // }, []);
+
   return (
     <div className={styles.wrapper}>
-        SidenavWrapper
-        </div>
-  )
+
+      <p>Dodaj zwierze</p>
+      <input type="text" placeholder="Nazwa zwierza" />
+      <button>
+        Dodaj
+      </button> 
+
+
+    </div>
+  );
 }
 
-export default SidenavWrapper
+export default SidenavWrapper;
